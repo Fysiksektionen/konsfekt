fe75c1357f217ee9c776c41fd71b70f84d1d80ab
Goby56 <60710855+Goby56@users.noreply.github.com>
2026-02-12 17:00:32 +0100

    /admin -> / -> /profil now works without unchanged layout

--- frontend/src/lib/components/LogoButton.svelte
+++ frontend/src/lib/components/LogoButton.svelte
@@ -2,6 +2,6 @@
     import { Button } from "./ui/button";
 </script>
 
-<Button class="text-3xl bg-primary text-background rounded-md p-2 text-shadow-2xs text-shadow-accent" href="/">
+<Button class="text-3xl bg-primary text-background rounded-md p-2 text-shadow-2xs text-shadow-accent" href="/" data-sveltekit-reload>
     <p class="text-3xl bg-primary text-background rounded-md p-2 text-shadow-2xs text-shadow-accent">Konsfekt</p>
 </Button>
